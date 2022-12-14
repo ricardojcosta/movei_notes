dcb94c407921365ce1e27cd03df706f816a9ab9b
Ricardo <ricardoredes2006@gmail.com>
2022-12-14 13:37:41 -0300

    Database/Migrations

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,58 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
+const { use } = require("express/lib/router");
+const { response } = require("express");
 
 class UsersController{
-  create (request, response){
+  async create (request, response){
      const { name, email, password } = request.body;
 
-     if(!name){
-      throw new AppError ("Nome é obrigatório");
+     const database = await sqliteConnection();
+     const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+     if(checkUserExists){
+      throw new AppError("Este e-mail já esta em uso.");
      }
 
-     response.status(201).json({ name, email, password });
+     const hashedPassword = await hash(password, 10);
+
+     await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
+
+     return response.status(201).json();
+    
+  }
+
+  async update(request, response){
+    const {name, email} = request.body;
+    const {id} = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if(!user){
+      throw new AppError("Usuário não encontrado!");
+    }
+
+    const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id){
+      throw new AppError("Este e-mail já esta em uso");
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(`
+    UPDATE users SET
+    name = ?,
+    email = ?,
+    updated_at = ?
+    WHERE id = ?`,
+    [user.name, user.email, new Date(), id]
+    );
+
+    return response.json();
   }
 }
 
