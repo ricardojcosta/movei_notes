7c7a2cd31fdeacf3e0dfb88aeadaa8e888cef557
Ricardo <ricardoredes2006@gmail.com>
2022-12-16 16:07:38 -0300

    crud 1

---
+++ src/controllers/MoviesNotesController.js
@@ -0,0 +1,65 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
+class MoviesNotesController{
+  async create(request, response){
+    const { title, description, rating, tags } = request.body;
+    const { user_id } = request.params;
+
+    if (rating > 5){
+      throw new AppError("O valor de rating deve ser de 1 até 5");
+    }
+
+      const note_id = await knex("movie_notes").insert({
+        title,
+        description,
+        rating,
+        user_id,
+      })
+
+
+    const tagsInsert = tags.map (name => {
+      return {
+        note_id,
+        name,
+        user_id
+      }
+    });
+
+    await knex("movie_tags").insert(tagsInsert);
+    
+    response.json();
+
+  }
+
+  async show(request, response){
+    const { id } = request.params;
+
+    const note = await knex("movie_notes").where({ id }).first();
+    const tags = await knex("movie_tags").where({note_id:id}).orderBy("name");
+
+    return response.json({
+      ...note,
+      tags
+    });
+  }
+
+  async delete(request, response){
+    const { id } = request.params;
+
+    await knex ("movie_notes").where({ id }).delete();
+
+    return response.json();
+  }
+
+  async index(request, response){
+    const { user_id } = request.query;
+
+    const notes = await knex("movie_notes").where({user_id}).orderBy("title");
+
+    return response.json(notes);
+  }
+
+}
+
+module.exports = MoviesNotesController;
