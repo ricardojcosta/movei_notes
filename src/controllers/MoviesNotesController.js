722031741b659d1f32edfcc5443be8603ee8d3b6
Ricardo <ricardoredes2006@gmail.com>
2022-12-19 15:07:29 -0300

    Api concuida

--- src/controllers/MoviesNotesController.js
+++ src/controllers/MoviesNotesController.js
@@ -52,13 +52,40 @@ class MoviesNotesController{
   }
 
   async index(request, response){
-    const { user_id } = request.query;
+    const { title, user_id, tags } = request.query;
 
-    const notes = await knex("movie_notes").where({user_id}).orderBy("title");
+    let notes;
 
-    return response.json(notes);
+    if(tags){
+      const filterTags = tags.split(',').map(tag =>tag.trim());
+
+      notes = await knex("movie_tags")
+        .select(["movie_notes.id", "movie_notes.title", "movie_notes.user_id"])
+        .where("movie_notes.user_id", user_id)
+        .whereLike("movie_notes.title", `%${title}%`)
+        .whereIn("name", filterTags)
+        .innerJoin("movie_notes", "movie_notes.id", "movie_tags.note_id")
+        .orderBy("title")
+
+    }else{
+      notes = await knex("movie_notes")
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title");
+    }
+
+    const userTags = await knex("movie_tags").where({user_id});
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    });
+
+    return response.json(notesWithTags);
   }
-
 }
 
 module.exports = MoviesNotesController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
 const notesRouter = require("./movies_notes.routes");
+const tagsRouter = require("./movies_tags.routes");
 
 const routes = Router();
 routes.use("/users", usersRouter);
 routes.use("/notes", notesRouter);
+routes.use("/tags", tagsRouter);
 
 module.exports = routes;
